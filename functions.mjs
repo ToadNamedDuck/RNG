826ce4215714086308292fac8370ca5b8c46b4a2
Jason Harris <lord.jtharris@gmail.com>
2023-08-24 21:36:40 -0500

    Added the functionality to select the maximum amount of syllables for the generated names.

--- functions.mjs
+++ functions.mjs
@@ -7,7 +7,6 @@ const races = [
 ]
 
 
-const rl = readline.createInterface({ input, output });
 //We want to have the user select options for generating the names in the future, but for now
 //we'll just ask them their name and spit it back out at them.
 //Every type of option we want to select - ie: races, syllables, etc should be passed in as a variable so we can modify their values.
@@ -17,7 +16,9 @@ function _raceNameGrabber(index) {
 }
 
 export async function SelectOptions(){
+    const rl = readline.createInterface({ input, output });
     let index;
+
     console.log("Here is a list of races for which you can generate a name for.");
     console.table(races)
     await rl.question("Please enter the index of the race you would like to generate names for.").then((input) => {
@@ -27,4 +28,40 @@ export async function SelectOptions(){
     .then(() => rl.close())
 
     return index;
+}
+
+function _inputCheck(input){
+
+        if(!Number.isNaN(parseInt(input))){   //If we parse int on input, and it is NOT NaN, then it is a number, and we return true;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+export async function SelectMaxSyllables(){
+    let max;
+    let enteredValidNumber = false;
+
+    while(true){
+        const rl = readline.createInterface({ input, output });
+        await rl.question("Please input the MAXIMUM NUMBER of syllables for your desired name.")
+        .then((input) => {
+            enteredValidNumber = _inputCheck(input)
+                if(enteredValidNumber){
+                    max = parseInt(input);
+                }
+                else{
+                    console.log("This is not a valid number! Enter an integer.")
+                }
+
+        })
+        .then(() => rl.close())
+        if(max !== undefined){
+            break;
+        }
+    }
+
+    return max;
 }
--- main.mjs
+++ main.mjs
@@ -1,13 +1,15 @@
 import names from "./name-fragments.json" assert {type: "json"}
-import { SelectOptions } from "./functions.mjs";
+import { SelectMaxSyllables, SelectOptions } from "./functions.mjs";
 
 async function main(){
     let preferredRaceVar;
     let maximumSyllablesVar;
     //Function that asks for user input
-    preferredRaceVar = await SelectOptions().then((index) => {preferredRaceVar = index;})
+    preferredRaceVar = await SelectOptions();
+    console.log(preferredRaceVar);
     //After input, the randomizer function
-    
+    maximumSyllablesVar = await SelectMaxSyllables()
+    console.log(maximumSyllablesVar);
 }
 
 main();
