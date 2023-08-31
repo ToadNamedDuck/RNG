0e1f3b3485e7d87a2ac9dfe849ea30b094cb3784
Jason Harris <lord.jtharris@gmail.com>
2023-08-31 15:05:56 -0500

    Working on re-architecting to grab the json file every cycle so updates can be used immediately

--- functions.mjs
+++ functions.mjs
@@ -1,17 +1,17 @@
 import * as readline from 'node:readline/promises';  // This uses the promise-based APIs for Node's native readline
 import { stdin as input, stdout as output } from 'node:process';
-import names from "./name-fragments.json" assert {type: "json"}
-import fs from "node:fs/promises";
+import fs, { readFile } from "node:fs/promises";
 
-const races = names.races;
+
 
 
 //We want to have the user select options for generating the names in the future, but for now
 //we'll just ask them their name and spit it back out at them.
 //Every type of option we want to select - ie: races, syllables, etc should be passed in as a variable so we can modify their values.
 
-function _raceNameGrabber(index) {
-    return races[index];
+async function _raceNameGrabber(index) {
+    let names = JSON.parse(await readFile("./name-fragments.json", "utf8"))
+    return names.races[index];
 }
 
 function _inputCheck(input) {
@@ -25,28 +25,28 @@ function _inputCheck(input) {
 }
 
 export async function AskWhatToDo() {
-    while(true){
-        const rl = readline.createInterface({input, output});
+    while (true) {
+        const rl = readline.createInterface({ input, output });
         let inputIsValid;
         let useCase;
         let options = ["Modify Data", "Use the Program", "Exit"]
         console.table(options);
         await rl.question("Type the corresponding index for what you would like this time around.\n")
-        .then((input) => {
-            inputIsValid = _inputCheck(input)
-            if(inputIsValid){
-                let selectedOption = options[parseInt(input)]
-                if(selectedOption !== undefined){
-                    useCase = selectedOption;
+            .then((input) => {
+                inputIsValid = _inputCheck(input)
+                if (inputIsValid) {
+                    let selectedOption = options[parseInt(input)]
+                    if (selectedOption !== undefined) {
+                        useCase = selectedOption;
+                    }
                 }
-            }
-            else{
-                console.log("Please select a valid option.\n")
-            }
-        })
-        .then(() => rl.close())
+                else {
+                    console.log("Please select a valid option.\n")
+                }
+            })
+            .then(() => rl.close())
 
-        if(useCase !== undefined){
+        if (useCase !== undefined) {
             return useCase;
         }
     }
@@ -55,24 +55,26 @@ export async function AskWhatToDo() {
 export async function SelectOptions() {
     let index;
     let inputIsValid = false;
+    let names = JSON.parse(await readFile("./name-fragments.json", "utf8"))
 
     console.log("Here is a list of races for which you can generate a name for.");
-    console.table(races);
+    console.table(names.races);
 
     while (true) {
         const rl = readline.createInterface({ input, output });
-        await rl.question("Please enter the index of the race you would like to generate names for.\n").then((input) => {
+        await rl.question("Please enter the index of the race you would like to generate names for.\n").then(async (input) => {
             inputIsValid = _inputCheck(input);
             if (inputIsValid) {
                 index = parseInt(input);
-                const race = _raceNameGrabber(index);
-                if (race !== undefined) {
-                    console.log("You selected: " + race + "\n");
-                }
-                else {
-                    console.log(`No race corresponds to input: ${input}\n`)
-                    inputIsValid = false;
-                }
+                await _raceNameGrabber(index).then((race) => {
+                    if (race !== undefined) {
+                        console.log("You selected: " + race + "\n");
+                    }
+                    else {
+                        console.log(`No race corresponds to input: ${input}\n`)
+                        inputIsValid = false;
+                    }
+                })
             }
             else {
                 console.log("This is not a valid number! Enter an integer.\n")
@@ -93,17 +95,16 @@ export async function SelectMaxSyllables() {
     let enteredValidNumber = false;
 
     while (true) {
-        const rl = readline.createInterface({ input, output });
+        const rl = readline.createInterface({ input, output, terminal: false });
         await rl.question("Please input the MAXIMUM NUMBER of syllables for your desired name. Minimum 1.\n")
             .then((input) => {
-                enteredValidNumber = _inputCheck(input)
+                enteredValidNumber = _inputCheck(input);
                 if (enteredValidNumber) {
                     max = parseInt(input);
                 }
                 else {
                     console.log("This is not a valid number! Enter an integer.\n")
                 }
-
             })
             .then(() => rl.close())
         if (max !== undefined) {
@@ -117,94 +118,95 @@ export async function SelectMaxSyllables() {
     return max;
 }
 
-export async function NumberOfNames(){
+export async function NumberOfNames() {
     let max;
-    let enteredValidNumber;
+    let enteredValidNumber = false;
 
-    while(true){
+    while (true) {
         const rl = readline.createInterface({ input, output });
         await rl.question("Please input the desired number of names to randomly generate.\n")
-        .then((input) => {
-            enteredValidNumber = _inputCheck(input);
-            if(enteredValidNumber){
-                max = parseInt(input)
-            }
-            else{
-                console.log("This is not a valid number! Enter an integer.\n")
-            }
-        })
-        .then(() => rl.close())
-        if(max !== undefined){
+            .then((input) => {
+                enteredValidNumber = _inputCheck(input);
+                if (enteredValidNumber) {
+                    max = parseInt(input)
+                }
+                else {
+                    console.log("This is not a valid number! Enter an integer.\n")
+                }
+            })
+            .then(() => {rl.close();rl.removeAllListeners()})
+        if (max !== undefined) {
             break;
         }
     }
 
-    if(max <= 0){
+    if (max <= 0) {
         max = 1
     }
     return max;
 }
 
-export async function GoAgain(numberOfNames, raceIndex){
+export async function GoAgain(numberOfNames, raceIndex) {
     let response;
-    while(true){
+    const race = await _raceNameGrabber(raceIndex)
+    while (true) {
         const rl = readline.createInterface({ input, output });
-        await rl.question(`Would you like to generate ${numberOfNames} more ${_raceNameGrabber(raceIndex)} names? Y/N\n`)
-        .then((input) => {
-            if(input.toLowerCase() === "y"){
-                response = true;
-            }
-            if(input.toLowerCase() === "n"){
-                response =  false;
-            }
-            else{
-                console.log("Please enter 'y' or 'n'\n")
-            }
-        })
-        .then(() => rl.close())
-        if(response === true){
+        await rl.question(`Would you like to generate ${numberOfNames} more ${race} names? Y/N\n`)
+            .then((input) => {
+                if (input.toLowerCase() === "y") {
+                    response = true;
+                }
+                if (input.toLowerCase() === "n") {
+                    response = false;
+                }
+                else {
+                    console.log("Please enter 'y' or 'n'\n")
+                }
+            })
+            .then(() => rl.close())
+        if (response === true) {
             return true;
         }
-        if(response === false){
-            console.log("Goodbye!")
+        if (response === false) {
             return false;
         }
     }
-    
+
 }
 
 //We need to be able to put the names into a table to display them.
 //We also want to honor the user's requests for the values they entered
 //Max number of names will be the length of the array we output, and we'll use it in our loop.
 //If we want to not have duplicate names, we don't increment i when a duplicate name is found in the array.
-export function GenerateNames(preferredRaceIndex, maxSyllables, numberOfNames, nameFragments){
+export async function GenerateNames(preferredRaceIndex, maxSyllables, numberOfNames, nameFragments) {
     console.log("Starting to generate names. Please be patient.")
+    const race = await _raceNameGrabber(preferredRaceIndex)
     let names = [];//Initialize an empty array
 
-    for(let i = 0; i < numberOfNames;){
+    for (let i = 0; i < numberOfNames;) {
         //We need to intialize some sort of randomizer
         //Lets store the objects we grab from the json file into a temporary array to access the syllable count of the objects.
         let firstFragmentNotFound = true; // used for loop
         const tempNameFragmentHolder = [];
 
-        while(firstFragmentNotFound === true){
+        while (firstFragmentNotFound === true) {
             const firstFragmentIndex = Math.floor(Math.random() * nameFragments.firstNameFrontFragments.length)//We will always generate at least 1 fragment.
             const firstFragment = nameFragments.firstNameFrontFragments[firstFragmentIndex]
-            if(!(firstFragment.syllables > maxSyllables) && firstFragment.race === _raceNameGrabber(preferredRaceIndex)){
+            if (!(firstFragment.syllables > maxSyllables) && firstFragment.race === race) {
                 tempNameFragmentHolder.push(firstFragment);
                 firstFragmentNotFound = false;
                 break;
             }
         }
-        if(tempNameFragmentHolder[0].syllables < maxSyllables){//If there are still available syllables, make a second fragment
+        if (tempNameFragmentHolder[0].syllables < maxSyllables) {//If there are still available syllables, make a second fragment
             let currentSyllables = tempNameFragmentHolder[0].syllables;
             let secondFragmentNotFound = true;
             let i = 0;
-            while(secondFragmentNotFound && i < 10){
+            while (secondFragmentNotFound && i < 10) {
                 const secondFragmentIndex = Math.floor(Math.random() * nameFragments.firstNameMiddleFragments.length)
                 const secondFragment = nameFragments.firstNameMiddleFragments[secondFragmentIndex];
 
-                if((secondFragment.syllables + currentSyllables ) <= maxSyllables && secondFragment.race === _raceNameGrabber(preferredRaceIndex)){
+                if ((secondFragment.syllables + currentSyllables) <= maxSyllables && secondFragment.race === race) {
                     tempNameFragmentHolder.push(secondFragment)
                     break;
                 }
@@ -215,56 +217,63 @@ export function GenerateNames(preferredRaceIndex, maxSyllables, numberOfNames, n
 
         //loop through the temporary array and combine name fragments, then push the name to the names array
         //Since we are only doing 2 fragments right now, we can simply add them without really looping. - If we add more, we will need to loop.
-        if(tempNameFragmentHolder[1] !== undefined){
+        if (tempNameFragmentHolder[1] !== undefined) {
             names.push(tempNameFragmentHolder[0].fragment + tempNameFragmentHolder[1].fragment);
         }
-        else{
+        else {
             names.push(tempNameFragmentHolder[0].fragment)
         }
-        console.log("Percent complete: " + Math.floor(((i + 1)/numberOfNames)*100) + "%")
+        console.log("Percent complete: " + Math.floor(((i + 1) / numberOfNames) * 100) + "%")
         i++;
     }
     console.table(names);
     return names;
 }
 
-export async function ModifyData(){
+export async function ModifyData() {
     const options = ["Add new race", "Delete race", "Add name fragments", "Go Back"];
     let selectedValidOption = false;
     let selectedOption;
-    while(!selectedValidOption){
+    while (!selectedValidOption) {
         console.table(options)
         const rl = readline.createInterface({ input, output });
         await rl.question("Select an option.").then((input) => {
             const inputIsValid = _inputCheck(input);
-            if(inputIsValid){
+            if (inputIsValid) {
                 selectedOption = options[parseInt(input)]
-                if(selectedOption === undefined){
+                if (selectedOption === undefined) {
                     console.log("Please select an option that exists.\n")
                 }
-                else{
+                else {
                     selectedValidOption = true;
                 }
             }
-            else{
+            else {
                 console.log("Please enter the number that corresponds to the option you wish to select.\n")
             }
         })
-        .then(() => rl.close())
+            .then(() => rl.close())
     }
-    switch(selectedOption){
+    switch (selectedOption) {
         case "Add new race": {
-
+            console.log("Adding new race\n")
+            break;
         }
         case "Delete race": {
-
+            console.log("Deleting race and associated names from database\n")
+            break;
         }
-        case "Add name fragments":{
-
+        case "Add name fragments": {
+            console.log("Adding name fragments for selected race\n")
+            break;
         }
         case "Go Back": {
             break;
         }
     }
+
+}
+
+async function _addRace(jsonFile) {
 
 }
--- main.mjs
+++ main.mjs
@@ -1,6 +1,5 @@
-import names from "./name-fragments.json" assert {type: "json"}
 import { AskWhatToDo, GenerateNames, GoAgain, ModifyData, NumberOfNames, SelectMaxSyllables, SelectOptions } from "./functions.mjs";
-
+import { readFile } from 'fs/promises';
 async function main() {
     let preferredRaceVar;
     let maximumSyllablesVar;
@@ -9,35 +8,39 @@ async function main() {
 
 
     while(!endLoop){
-        switch (await AskWhatToDo()) {
-            case 'Use the Program': {
-                //Functions that asks for user input
-                preferredRaceVar = await SelectOptions();
-                maximumSyllablesVar = await SelectMaxSyllables();
-                numberOfNames = await NumberOfNames();
-                console.log("Your preferred values have been saved for this session and will be used to generate names.")
-                //After input, the randomizer function, which takes our preferred race, max syllables, number of names, and the names json as parameters
-                //Loop until user says they don't want to generate any more names. Ask for input after the names are generated.
-    
-                while (true) {
-                    GenerateNames(preferredRaceVar, maximumSyllablesVar, numberOfNames, names)
-                    const goAgain = await GoAgain(numberOfNames, preferredRaceVar);
-                    if (!goAgain) {
-                        break;
+        let names;
+        await readFile("./name-fragments.json", "utf8").then((file) => names = JSON.parse(file))
+        .then(async () => {
+            switch (await AskWhatToDo()) {
+                case 'Use the Program': {
+                    //Functions that asks for user input
+                    await SelectOptions().then((input) => {preferredRaceVar = input})
+                    .then(async() => await SelectMaxSyllables().then((input) => {maximumSyllablesVar = input}))
+                    .then(async() => await NumberOfNames().then((input) => {numberOfNames = input}))
+                    console.log("Your preferred values have been saved for this session and will be used to generate names.")
+                    //After input, the randomizer function, which takes our preferred race, max syllables, number of names, and the names json as parameters
+                    //Loop until user says they don't want to generate any more names. Ask for input after the names are generated.
+        
+                    while (true) {
+                        GenerateNames(preferredRaceVar, maximumSyllablesVar, numberOfNames, names)
+                        const goAgain = await GoAgain(numberOfNames, preferredRaceVar);
+                        if (!goAgain) {
+                            break;
+                        }
                     }
+                    break;
                 }
-                break;
+                case 'Modify Data': {
+                    await ModifyData();
+                    break;
+                }
+                case 'Exit': {
+                    console.log("See you again soon.")
+                    endLoop = true;
+                    break;
+                }
             }
-            case 'Modify Data': {
-                await ModifyData();
-                break;
-            }
-            case 'Exit': {
-                console.log("See you again soon.")
-                endLoop = true;
-                break;
-            }
-        }
+        })
     }
 }
 
